61644753e279de2afc7979e00b76d7ae0ff6a012
Rahul Rakshit <rahul.rakshit@posteo.net>
2026-05-13 11:15:08 +0200

    Update build command to not use cache

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cp ~/.tmux.conf "$SCRIPT_DIR/.tmux.conf"
 rsync -a --exclude='.git' ~/.tmux/ "$SCRIPT_DIR/.tmux/"
 
-docker build -t ai_sandbox "$SCRIPT_DIR"
+docker build --no-cache -t ai_sandbox "$SCRIPT_DIR"
